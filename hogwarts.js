b0a1a30c29368ac8b638fcee9621168e7bdb8270
Klebkim <132722047+Klebkim@users.noreply.github.com>
2024-06-08 20:40:53 -0500

    Update hogwarts.js

--- hogwarts.js
+++ hogwarts.js
@@ -1,7 +1,26 @@
-var questionNum = 1
-
+var questionNum = 1;
+var slytherinScore = 0;
+var ravenclawScore = 0;
+var gryffindorScore = 0;
+var hufflepuffScore = 0;
 function updateQuestionNum(){
 let qNum = document.getElementById("q-num")
     qNum.innerText = questionNum;
 
+}
+function updateslytherinScore(){
+
+    slytherinScore ++;
+}
+function updateravenclawScore(){
+
+    ravenclawScore ++;
+    }
+function updategryffindorScore(){
+
+    gryffindorScore ++;
+}
+function updatehufflepuffScore(){
+
+    hufflepuffscore ++;
 }
